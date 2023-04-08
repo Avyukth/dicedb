3167fbf7cc3d8ec28b8223c14bedb03b63e6af64
Avyukth <makur.subhrajit@gmail.com>
2023-04-08 18:37:55 +0530

    setup flags added

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
+package main
+
 import (
 	"flag"
 	"log"
-	"github.com/dicedb/dice/config"
-	"github.com/dicedb/dice/server"
+	"github.com/Avyukth/dicedb/config"
+	"github.com/Avyukth/dicedb/server"
 )
+
+func setupFlags(){
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "Host for the dice server")
+	flag.StringVar(&config.Port, "port", 7379, "port for the dice server")
+	flag.Parse()
+}
 
 func main() {
 	setupFlags()
